4b464b469bebb6426dbb520b3a85ba65666152d4
Fabiola <fabi.ampuero.chalco@gmail.com>
2016-12-21 07:54:03 -0500

    esturctura de html y js al 98%

---
+++ main.js
@@ -0,0 +1,21 @@
+var puntajeSquad = document.getElementById("puntaje_squad");
+var puntajeMax = document.getElementById("puntaje_max");
+var divSalida = document.getElementById ("salida");
+var puntajeFinal = 0;
+
+function calcular () {
+    var valorPuntajeSquad = puntajeSquad.value;
+    var valorPuntajeMax = puntajeMax.value;
+    var relacion = parseFloat(puntajeSquad.value)/parseFloat(puntajeMax.value);
+    var porcentaje = relacion*100;
+    
+    if (valorPuntajeSquad == "" || valorPuntajeMax == ""){
+        salida.innerHTML = "0.00";
+        document.getElementById("mensajes").innerHTML = "<div class='alert alert-danger'>No se ingresaron valores</div>";
+    }
+    else {
+        console.log("notas");
+        puntajeFinal = porcentaje;
+        salida.innerHTML = puntajeFinal.toFixed(2);
+    }
+}
