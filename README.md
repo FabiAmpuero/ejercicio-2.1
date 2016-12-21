2ca2314a32bcdd600e2a67196f814c271a6b9af4
Fabiola Ampuero <fabi.ampuero.chaclco@gmail.com>
2016-12-21 10:52:37 -0500

    ejercicio terminado

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # ejercicio-2.1
+
+Ejercicio 5
+
+*Inicio: Calcular el porcentaje del squad*
+
+## Ingreso de datos
+- punto_squad
+- max_puntos
+- porcentaje_squad
+
+## Proceso
+- Pedir punto_squad
+- Pedir max_puntos
+- Operar (100 * punto_squad) / max_puntos y guardar en porcentaje squad
+- Si porcentaje_squad es mayor que 80
+    - Mostrar mensaje "Muy bien squad!"
+- Si no 
+    - Mostrar mensaje "Pueden hacerlo mejor"
+
+*Fin*
--- main.js
+++ main.js
@@ -1,21 +1,12 @@
-var puntajeSquad = document.getElementById("puntaje_squad");
-var puntajeMax = document.getElementById("puntaje_max");
-var divSalida = document.getElementById ("salida");
-var puntajeFinal = 0;
-
-function calcular () {
-    var valorPuntajeSquad = puntajeSquad.value;
-    var valorPuntajeMax = puntajeMax.value;
-    var relacion = parseFloat(puntajeSquad.value)/parseFloat(puntajeMax.value);
-    var porcentaje = relacion*100;
+function calcular() {
+    var puntajeSquad = document.getElementById("puntaje_squad").value;
+    var puntajeMax = document.getElementById("puntaje_max").value;
+    var porcentajeSquad = (100*puntajeSquad)/puntajeMax;
     
-    if (valorPuntajeSquad == "" || valorPuntajeMax == ""){
-        salida.innerHTML = "0.00";
-        document.getElementById("mensajes").innerHTML = "<div class='alert alert-danger'>No se ingresaron valores</div>";
+    if(porcentajeSquad >= 80) {
+        document.getElementById("salida").innerHTML = "Muy bien squad!";
     }
     else {
-        console.log("notas");
-        puntajeFinal = porcentaje;
-        salida.innerHTML = puntajeFinal.toFixed(2);
+        document.getElementById("salida").innerHTML = "Pueden hacerlo mejor";
     }
 }
